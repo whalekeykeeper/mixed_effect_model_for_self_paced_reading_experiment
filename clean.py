25f1964422cad1cb8142df2ba5106a5406643d1e
whalekeykeeper <46596921+whalekeykeeper@users.noreply.github.com>
2022-03-13 21:43:17 +0100

    backup

--- clean.py
+++ clean.py
@@ -9,23 +9,7 @@ def read_csv(path):
     return df
 
 
-def remove_filler(df):
-    df = df[~df.itemID.str.contains('f')]
-    df = df.astype({"itemID": int})
-    return df
-
-
-def check_items(df):
-    items = df.itemID.unique().tolist()
-    items.sort()
-    print("All the items are: ", items)
-
-
-def save_csv(save_path):
-    df.to_csv(save_path)
-
-
-def get_average_accuracy(df):
+def get_averaged_accuracy(df):
     # This function should be used in the raw csv because "response" column is removed in the processed one.
     response_count = 0
     correct_count = 0
@@ -35,7 +19,42 @@ def get_average_accuracy(df):
             if row['correct_answer'] == row['response']:
                 correct_count += 1
     accuracy = correct_count / response_count
-    print("The global accuracy is: ", accuracy)
+    print("The averaged accuracy is: ", accuracy)
+
+
+def remove_filler(df):
+    df = df[~df.itemID.str.contains('f')]
+    df = df.astype({"itemID": int})
+    return df
+
+
+def get_items(df):
+    items = df.itemID.unique().tolist()
+    items.sort()
+    return items
+
+def save_csv(save_path):
+    df.to_csv(save_path)
+
+
+def filter_individual_accuracy(df):
+    submissions = df.submission_id.unique()
+    items_count = len(get_items(df))
+    correct_count = 0
+    submissions_new = []
+    for s in submissions:
+        for index, row in df.iterrows():
+            if isinstance(row['submission_id'], int) and row['submission_id'] == s:
+                if row['correct_answer'] == row['response']:
+                    correct_count += 1
+        accuracy = correct_count / items_count
+        print("The local accuracy for participant", s, "is: ", accuracy)
+        if accuracy >= 0.8:
+            submissions_new.append(s)
+        correct_count = 0
+    df = df[df['submission_id'].isin(submissions_new)]
+    print(df)
+    return df
 
 
 if __name__ == "__main__":
@@ -46,13 +65,17 @@ if __name__ == "__main__":
     df = read_csv(csv_path)
 
     """Get the average accuracy for all the participants"""
-    get_average_accuracy(df)
+    get_averaged_accuracy(df)
+
+    """Get individual accuracy, only keep those bigger than 80%"""
+    df = filter_individual_accuracy(df)
 
     """Remove all the filler sentences"""
     df = remove_filler(df)
 
     """Get all the items"""
-    check_items(df)
+    items = get_items(df)
+    print("All the items are: ", items)
 
     """Save the processed csv file"""
     save_csv(save_path)
@@ -60,25 +83,9 @@ if __name__ == "__main__":
 
 
 
-    # """
-    # Check if accuracy > 80%
-    # """
-    # correct_count = 0
-    # submissions_new = []
-    # for s in submissions:
-    #     for index, row in df.iterrows():
-    #         if isinstance(row['submission_id'], int) and row['submission_id'] == s:
-    #             if row['correct_answer'] == row['response']:
-    #                 correct_count += 1
-    #     accuracy = correct_count / items_count
-    #     print("The local accuracy for participant", s, "is: ", accuracy)
-    #     if accuracy >= 0.8:
-    #         submissions_new.append(s)
-    #     correct_count = 0
-    # print(submissions_new)
-    # print("____________________________")
-    #
-    # """
+
+
+    """
     # Extreme individual RTs (greater than 1,500 ms or less than 100 ms) were trimmed (0.3% of all data).
     # """
     # for index, row in df.iterrows():
