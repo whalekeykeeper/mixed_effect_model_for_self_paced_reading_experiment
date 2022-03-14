eedd079b7ef23adc920e9c0d7818f4d94d5561c0
whalekeykeeper <46596921+whalekeykeeper@users.noreply.github.com>
2022-03-14 03:01:59 +0100

    finished preprocessing

--- data_processing.py
+++ data_processing.py
@@ -73,56 +73,57 @@ def create_columns_word_number_word_rt(df):
         df2.at[i, 'word_number'] = count
         count += 1
 
-    df2 = pd.concat([df['phrase'], df2], axis=1)
-
     df3 = df.drop('times', axis=1).join(df['times'].str.split('|', expand=True).stack().reset_index(level=1,
                                                                                                     drop=True).rename(
         'rt'))
-    df3 = df3['rt']
+    df3 = df3[['rt', 'phrase']]
     df3 = df3.reset_index()
 
     return pd.concat([df2, df3], axis=1)
 
 
 def create_region(df):
-    df['region'] = "-"
+    df['region'] = ""
     for i, row in df.iterrows():
         if row['setting'] == 'focused':
             if row['word_number'] == 1:
-                df.at[i, 'region'] = 'focus_particle'
+                df.at[i, 'region'] = 'f_particle'
             elif row['word_number'] in [2, 3]:
-                df.at[i, 'region'] = 'quantifier_f'
+                df.at[i, 'region'] = 'f_quantifier'
             elif row['word_number'] in [4, 5]:
-                df.at[i, 'region'] = 'ntw_1'
+                df.at[i, 'region'] = 'f_ntw_1'
             elif row['word_number'] in [6, 7]:
-                df.at[i, 'region'] = 'ntw_2'
+                df.at[i, 'region'] = 'f_ntw_2'
+
         elif row['setting'] == 'scalar':
             if row['word_number'] in [1, 2]:
-                df.at[i, 'region'] = 'quantifier_s'
+                df.at[i, 'region'] = 's_quantifier'
             elif row['word_number'] in [3, 4]:
-                df.at[i, 'region'] = 'ntw_1'
+                df.at[i, 'region'] = 's_ntw_1'
             elif row['word_number'] in [5, 6]:
-                df.at[i, 'region'] = 'ntw_2'
+                df.at[i, 'region'] = 's_ntw_2'
+
         elif row['setting'] == 'cancelation':
             if row['word_number'] in [1, 2]:
-                df.at[i, 'region'] = 'in_fact'
+                df.at[i, 'region'] = 'cancelation_1'
             elif row['word_number'] in [3, 4]:
-                df.at[i, 'region'] = 'they_all'
+                df.at[i, 'region'] = 'cancelation_2'
             elif row['word_number'] == 5:
-                df.at[i, 'region'] = 'did'
+                df.at[i, 'region'] = 'cancelation_3'
             elif row['word_number'] == 6:
-                df.at[i, 'region'] = 'word6'
+                df.at[i, 'region'] = 'cancelation_4'
+
         elif row['setting'] == 'complement':
             if row['word_number'] in [1, 2]:
-                df.at[i, 'region'] = 'anaphor'
+                df.at[i, 'region'] = 'c_anaphor'
 
             if row['itemID'] in [2, 10, 21, 23, 24]:
                 if row['word_number'] in [3, 4, 5]:
-                    df.at[i, 'region'] = 'predicate'
+                    df.at[i, 'region'] = 'c_predicate'
                 elif row['word_number'] == 6:
-                    df.at[i, 'region'] = 'clause_boundary'
+                    df.at[i, 'region'] = 'c_clause_boundary'
                 elif row['word_number'] in [7, 8]:
-                    df.at[i, 'region'] = 'ntw_1'
+                    df.at[i, 'region'] = 'c_ntw_1'
 
             elif row['itemID'] == 4:
                 if row['word_number'] in [3, 4, 5, 6]:
@@ -130,7 +131,7 @@ def create_region(df):
                 elif row['word_number'] == 7:
                     df.at[i, 'region'] = 'clause_boundary'
                 elif row['word_number'] in [8, 9]:
-                    df.at[i, 'region'] = 'ntw_1'
+                    df.at[i, 'region'] = 'c_ntw_1'
 
             elif row['itemID'] == 8:
                 if row['word_number'] in [3, 4, 5, 6, 7, 8, 9, 10]:
@@ -138,7 +139,7 @@ def create_region(df):
                 elif row['word_number'] == 11:
                     df.at[i, 'region'] = 'clause_boundary'
                 elif row['word_number'] in [12, 13]:
-                    df.at[i, 'region'] = 'ntw_1'
+                    df.at[i, 'region'] = 'c_ntw_1'
 
             elif row['itemID'] == 20:
                 if row['word_number'] in [3, 4, 5, 6, 7]:
@@ -146,7 +147,7 @@ def create_region(df):
                 elif row['word_number'] == 8:
                     df.at[i, 'region'] = 'clause_boundary'
                 elif row['word_number'] in [9, 10]:
-                    df.at[i, 'region'] = 'ntw_1'
+                    df.at[i, 'region'] = 'c_ntw_1'
 
             else:
                 if row['word_number'] in [3, 4]:
@@ -154,8 +155,41 @@ def create_region(df):
                 elif row['word_number'] == 5:
                     df.at[i, 'region'] = 'clause_boundary'
                 elif row['word_number'] in [6, 7]:
-                    df.at[i, 'region'] = 'ntw_1'
+                    df.at[i, 'region'] = 'c_ntw_1'
 
+    return df
+
+
+
+def create_average_rt(df):
+    df2 = df.astype({"rt": int})
+
+    df2['avg_rt'] = df2.groupby(['submission_id', 'itemID', 'setting', 'region']).rt.transform('mean')
+
+    df3 = df2[['submission_id', 'itemID', 'type', 'setting', 'region', 'avg_rt',
+               'phrase', 'phrase_length']]
+    df4 = df3.drop_duplicates()
+    return df4
+
+
+def full_or_partial(df):
+    df['full_or_partial'] = ''
+    for i, row in df.iterrows():
+        if row['type'] == 'context':
+            full_or_partial = row['setting']
+        df.at[i, 'full_or_partial'] = full_or_partial
+
+    df2 = df[df['region'].str.len() > 0]
+    df2 = df2.reset_index()
+    df2 = df2.drop('index', 1)
+    return df2
+
+
+def quantifier_or_not(df):
+    df['quantifier'] = ''
+    for i, row in df.iterrows():
+        if row['region'] in ['s_quantifier', 'f_quantifier']:
+            df.at[i, 'quantifier'] = 'quantifier'
     return df
 
 
@@ -188,13 +222,30 @@ if __name__ == "__main__":
     df = create_region(df)
 
     """Only keep necessary columns"""
-    df_processed = df[['setting', 'submission_id', 'word_number', 'word', 'rt', 'phrase', 'region',
-                       'type', 'phrase_length']]
+    df_processed = df[['setting', 'submission_id', 'word_number', 'word', 'rt', 'region', 'type', 'itemID', 'phrase',
+                       'phrase_length']]
+    df_processed = df_processed.reset_index()
 
-    print(df_processed)
+    print("The intermediate-processed table have the following columns: \n", df_processed.columns)
+
+    """Get average rt for regions"""
+    df_processed_for_avg_rt = create_average_rt(df_processed)
+
+    """full_or_partial"""
+    df_full_or_partial = full_or_partial(df_processed_for_avg_rt)
+
+
+    """quantifier region or not"""
+    df_final = quantifier_or_not(df_full_or_partial)
+    print(df.region.unique())
+
 
     """Save the processed csv file"""
-    df_processed.to_csv(save_path)
+    print("The final table have the following columns: \n", df_final.columns)
+    df_final.to_csv(save_path)
+
+
+
 
 
     """
