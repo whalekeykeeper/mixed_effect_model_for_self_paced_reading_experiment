9bd81f1b7435be57423df68be9ab3d066ca14a27
whalekeykeeper <46596921+whalekeykeeper@users.noreply.github.com>
2022-03-31 19:15:35 +0200

    4 figures

---
+++ get_firgures.R
@@ -0,0 +1,60 @@
+library(dplyr)
+library(ggplot2)
+library(Scales)
+
+# read data
+df = read.csv("https://raw.githubusercontent.com/whalekeykeeper/mixed_effect_model_for_self_paced_reading_experiment/main/data_processed.csv")
+head(df)
+
+
+# Firgure 1
+scalar_trigger_sentences <- df %>%
+  filter(setting == "scalar") %>%
+  group_by(speakerKnowledge, region)  %>% 
+  summarize(m = mean(avg_rt)) 
+head(scalar_trigger_sentences)
+# Plot
+scalar_trigger_sentences %>%
+  ggplot( aes(x=region, y=m, group=speakerKnowledge, color=speakerKnowledge)) +
+  geom_line(aes(linetype=speakerKnowledge)) + geom_point()+
+  scale_y_continuous(breaks = pretty(scalar_trigger_sentences$m, n = 10))
+
+
+# Firgure 2
+focused_trigger_sentences <- df %>%
+  filter(setting == "focused") %>%
+  group_by(speakerKnowledge, region)  %>% 
+  summarize(m = mean(avg_rt)) 
+head(focused_trigger_sentences)
+# Plot
+focused_trigger_sentences %>%
+  ggplot( aes(x=region, y=m, group=speakerKnowledge, color=speakerKnowledge)) +
+  geom_line(aes(linetype=speakerKnowledge)) + geom_point()+
+  scale_y_continuous(breaks = pretty(focused_trigger_sentences$m, n = 10))
+
+
+# Firgure 3
+complement_with_focused_trigger <- df %>%
+  filter(setting == "complement", triggerType == "focused") %>%
+  group_by(speakerKnowledge, region)  %>% 
+  summarize(m = mean(avg_rt)) 
+head(complement_with_focused_trigger)
+# Plot
+complement_with_focused_trigger %>%
+  ggplot( aes(x=region, y=m, group=speakerKnowledge, color=speakerKnowledge)) +
+  geom_line(aes(linetype=speakerKnowledge)) + geom_point()+
+  scale_y_continuous(breaks = pretty(complement_with_focused_trigger$m, n = 10))
+
+
+# Firgure 4
+cancelation <- df %>%
+  filter(setting == "cancelation") %>%
+  group_by(speakerKnowledge, region)  %>% 
+  summarize(m = mean(avg_rt)) 
+head(cancelation)
+# Plot
+cancelation %>%
+  ggplot( aes(x=region, y=m, group=speakerKnowledge, color=speakerKnowledge)) +
+  geom_line(aes(linetype=speakerKnowledge)) + geom_point() +
+  scale_y_continuous(breaks = pretty(cancelation$m, n = 10))
+
